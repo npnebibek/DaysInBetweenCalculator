10790511662cb307d2227cadb73c877f89392b4a
Bibek Neupane <porcupinedev@gmail.com>
2023-10-07 18:56:19 +1100

    Add project files.

---
+++ DaysInBetweenCalculator/Implementaion/BusinessDayCounter.cs
@@ -0,0 +1,53 @@
+﻿using DaysInBetweenCalculator.Interface;
+
+namespace DaysInBetweenCalculator.Implementaion
+{
+    public class BusinessDayCounter : IBusinessDayCounter
+    {
+        /// <summary>
+        /// Calculate number of weekdays between two days
+        /// </summary>
+        /// <param name="firstDate"></param>
+        /// <param name="secondDate"></param>
+        /// <returns></returns>
+        public int WeekdaysBetweenTwoDates(DateTime firstDate, DateTime secondDate)
+        {
+            ValidateInputDates(firstDate, secondDate);
+            return 0;
+        }
+
+        /// <summary>
+        /// Calculate number of business days between two dates based on public holidays
+        /// </summary>
+        /// <param name="firstDate"></param>
+        /// <param name="secondDate"></param>
+        /// <param name="publicHolidays"></param>
+        /// <returns></returns>
+        public int BusinessDaysBetweenTwoDates(DateTime firstDate, DateTime secondDate, IEnumerable<DateTime> publicHolidays)
+        {
+            ValidateInputDates(firstDate, secondDate);
+            return 0;
+        }
+
+        /// <summary>
+        /// Validate entered date values
+        /// </summary>
+        /// <param name="firstDate"></param>
+        /// <param name="secondDate"></param>
+        /// <returns></returns>
+        private static int ValidateInputDates(DateTime firstDate, DateTime secondDate)
+        {
+
+            if (DateTime.Compare(firstDate, secondDate) > 0)
+            {
+                return 0;
+            }
+            else if (DateTime.Compare(firstDate, secondDate) == 0)
+            {
+                return 0;
+            }
+            return 1;
+        }
+
+    }
+}
---
+++ DaysInBetweenCalculator/Interface/IBusinessDayCounter.cs
@@ -0,0 +1,9 @@
+﻿namespace DaysInBetweenCalculator.Interface
+{
+    internal interface IBusinessDayCounter
+    {
+        int WeekdaysBetweenTwoDates(DateTime firstDate, DateTime secondDate);
+        int BusinessDaysBetweenTwoDates(DateTime firstDate, DateTime secondDate, IEnumerable<DateTime> publicHolidays);
+
+    }
+}
