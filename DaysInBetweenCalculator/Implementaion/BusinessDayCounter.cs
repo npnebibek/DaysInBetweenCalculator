0a2bfd8a832d47e455ee908216af5b962401384c
Bibek Neupane <porcupinedev@gmail.com>
2023-10-07 20:00:14 +1100

    Calculate isBusinessDays

--- DaysInBetweenCalculator/Implementaion/BusinessDayCounter.cs
+++ DaysInBetweenCalculator/Implementaion/BusinessDayCounter.cs
@@ -4,6 +4,8 @@ namespace DaysInBetweenCalculator.Implementaion
 {
     public class BusinessDayCounter : IBusinessDayCounter
     {
+
+        #region public methods
         /// <summary>
         /// Calculate number of weekdays between two days
         /// </summary>
@@ -12,16 +14,26 @@ namespace DaysInBetweenCalculator.Implementaion
         /// <returns></returns>
         public int WeekdaysBetweenTwoDates(DateTime firstDate, DateTime secondDate)
         {
-            var numberOfDaysInBetween = ValidateInputDates(firstDate, secondDate);
+            var numberOfWeedays = 0;
 
-            if (numberOfDaysInBetween == 0)
+            if (!IsValidDateInputs(firstDate, secondDate))
             {
-                return numberOfDaysInBetween;
+                return numberOfWeedays;
             }
 
+            //We do not include the startDate and endDate
+            var currentDate = firstDate.AddDays(1);
 
+            while(currentDate < secondDate)
+            {
+                if(IsWeekday(currentDate))
+                {
+                    numberOfWeedays++;
+                }
+                currentDate = currentDate.AddDays(1);
+            }
 
-            return numberOfDaysInBetween;
+            return numberOfWeedays;
         }
 
         /// <summary>
@@ -33,32 +45,66 @@ namespace DaysInBetweenCalculator.Implementaion
         /// <returns></returns>
         public int BusinessDaysBetweenTwoDates(DateTime firstDate, DateTime secondDate, IEnumerable<DateTime> publicHolidays)
         {
-            var numberOfDaysInBetween = ValidateInputDates(firstDate, secondDate);
+            var numberOfBusinessDays = 0;
 
-            if (numberOfDaysInBetween == 0)
+            if (!IsValidDateInputs(firstDate, secondDate))
             {
-                return numberOfDaysInBetween;
+                return numberOfBusinessDays;
+            }
+
+            //We do not include the startDate and endDate
+            var currentDate = firstDate.AddDays(1);
+
+            while (currentDate < secondDate)
+            {
+                if (IsWeekday(currentDate))
+                {
+                    numberOfBusinessDays++;
+                }
+                currentDate = currentDate.AddDays(1);
             }
 
 
-
-            return numberOfDaysInBetween;
+            return numberOfBusinessDays;
         }
+        #endregion
 
+        #region private methods
         /// <summary>
         /// Validate entered date values
         /// </summary>
         /// <param name="firstDate"></param>
         /// <param name="secondDate"></param>
         /// <returns></returns>
-        private static int ValidateInputDates(DateTime firstDate, DateTime secondDate)
+        private static bool IsValidDateInputs(DateTime firstDate, DateTime secondDate)
         {
             if (DateTime.Compare(firstDate, secondDate) > 0 || DateTime.Compare(firstDate, secondDate) == 0)
             {
-                return 0;
+                return false;
             }
-            return 1;
+            return true;
         }
+
+        /// <summary>
+        /// Check Is Weekday?
+        /// </summary>
+        /// <param name="currentDate"></param>
+        /// <returns></returns>
+        private static bool IsWeekday(DateTime currentDate)
+        {
+            if(currentDate.DayOfWeek != DayOfWeek.Saturday || currentDate.DayOfWeek != DayOfWeek.Sunday)
+            {
+                return true;
+            }
+            return false;
+        }
+
+        private static bool IsBusinessDay(DateTime currentDate)
+        {
+            return false;
+        }
+
+        #endregion
 
     }
 }
